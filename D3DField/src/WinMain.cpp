0e89f575a10fd50ff29c1fc697974abf4914e83a
arajinzz <aladdinn360@gmail.com>
2022-10-21 21:41:52 +0100

    doing some basic configuration, and declaring a Win32 entry point

---
+++ D3DField/src/WinMain.cpp
@@ -0,0 +1,13 @@
+// This is the entry point
+
+#include <Windows.h>
+
+int WINAPI WinMain(
+    _In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPSTR lpCmdLine,
+    _In_ int nCmdShow
+)
+{
+    return 0;
+}
