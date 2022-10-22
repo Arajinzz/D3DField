a0e03d5a865959e04617d4e1bb66c0c4553c7776
arajinzz <aladdinn360@gmail.com>
2022-10-22 06:22:24 +0100

    Creating a window class and a window instance

--- D3DField/src/WinMain.cpp
+++ D3DField/src/WinMain.cpp
@@ -9,5 +9,44 @@ int WINAPI WinMain(
     _In_ int nCmdShow
 )
 {
+
+    const LPCWSTR className = L"D3DField";
+
+    // Register a window class
+    WNDCLASSEX wndClass = {0};
+    wndClass.cbSize = sizeof(wndClass);
+    wndClass.style = CS_OWNDC;
+    wndClass.lpfnWndProc = DefWindowProc;
+    wndClass.cbClsExtra = 0;
+    wndClass.cbWndExtra = 0;
+    wndClass.hInstance = hInstance;
+    wndClass.hIcon = nullptr;
+    wndClass.hIconSm = nullptr;
+    wndClass.hCursor = nullptr;
+    wndClass.hbrBackground = nullptr;
+    wndClass.lpszMenuName = nullptr;
+    wndClass.lpszClassName = className;
+    RegisterClassEx(&wndClass);
+
+    // Create a window instance
+    HWND hWnd = CreateWindowEx(
+        0,
+        className,
+        L"D3DField",
+        WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
+        100,
+        100,
+        800,
+        600,
+        nullptr,
+        nullptr,
+        hInstance,
+        nullptr
+    );
+
+    // Test window display, To be removed later
+    ShowWindow(hWnd, SW_SHOW);
+    while (true);
+
     return 0;
 }
